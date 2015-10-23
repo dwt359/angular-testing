8996aced5e6f65e405c57ad753c2ab550e767236
Daniel Troiani <dtroiani9@gmail.com>
2015-10-22 19:37:19 -0500

    views

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-angular.module('gameApp', [])
-    .controller('GameListController', function() {
-        var gameList = this;
-        gameList.games = [
-            {name: "Legend of Zelda: Ocarina of Time", system: "N64"},
-            {name: "Portal", system: "PC"},
-            {name: "Gladius", system: "Gamecube"},
-            {name: "Pikmin", system: "Gamecube"}
-        ];
+gameApp = angular.module('gameApp', ['ngRoute']);
 
-    });
+gameApp.controller('GameListController', function($scope) {
+    $scope.games = [
+        {gameId: 1, name: "Legend of Zelda: Ocarina of Time", system: "N64", desc: "Link saves zelda and stuff"},
+        {gameId: 2, name: "Portal", system: "PC", desc: "Shoot some holes in walls to solve puzzles"},
+        {gameId: 3, name: "Gladius", system: "Gamecube", desc: "Like fire emblem but weirder and awesome"},
+        {gameId: 4, name: "Pikmin", system: "Gamecube", desc: "Command a bunch of carrot-shaped weirdos as a tiny spaceman"}
+    ];
+});
+gameApp.controller('GameViewController', function($scope){
+
+});
+
+gameApp.config(function($routeProvider) {
+    $routeProvider.
+        when('/home', {
+            templateUrl: 'partials/home.html',
+            controller: 'GameListController'
+        }).
+        when('/game/:gameId', {
+            templateUrl: 'partials/game.html',
+            controller: 'GameViewController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+});
+
+
+
